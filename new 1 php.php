e77c79a970a29387674bdb4918a095d037d340e2
milarosyidauswatunnisa <48146032+milarosyidauswatunnisa@users.noreply.github.com>
2019-06-11 22:00:45 +0700

    form respon

---
+++ new 1 php.php
@@ -0,0 +1,20 @@
+<? php
+/*echo "Universitas Sebelas Maret<br>";
+Echo "Universitas Sebelas Maret<br>";
+ecHo "Universitas Sebelas Maret<br>";
+
+$prodi = "informatika";
+echo "Prodi saya $prodi <br>";
+echo "Prodi teman saya di " . $prodi . " <br>"; */
+
+$x = 100;
+$y = 92;
+
+function pertambahan() {
+	return $GLOBALS['a] = $GLOBALS['x'] + $GLOBALS['y];
+}
+var_dump(pertambahan());
+pertambahan();
+
+
+?>
